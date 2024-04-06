a413a89986dd9a4811ea4c985e09828e9d8923e6
sahanyaL <132870882+sahanyaL@users.noreply.github.com>
2024-04-06 18:09:22 +0530

    edited footer.jsx

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -170,53 +170,7 @@ function Footer() {
           </ul>
         </div>
       </div>
-      <hr className="divider" />
-      <div className="footer--content--container">
-        <p className="footer--content">Made with 💖 by John</p>
-        <div className="footer--social--icon">
-          <ul>
-            <li>
-              <Link
-                activeClass="navbar--active-content"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="Privacy_Policy"
-                className="text-sm"
-              >
-                Privacy Policy
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="navbar--active-content"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="Terms_of_Service"
-                className="text-sm"
-              >
-                Terms of Service
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="navbar--active-content"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="Cookies_Settings"
-                className="text-sm"
-              >
-                Cookies Settings
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+     
     </footer>
   );
 }
